c770fc4658bd39463d5a2aa9d974ab188a52c7a9
김민재 <lake041@korea.ac.kr>
2023-11-13 01:21:55 +0900

    HTML/CSS: 컴포넌트화 완료 (#3)
    ## Key Changes
    
    - index.js: 기존의 index.js를 animate.js로 이름을 바꾸고, index.js에서
    include-html.js와 animate.js를 순차적으로 호출하는 것으로 수정했다.
    - include-html.js: 부모의 innerHTML에 불러온 요소를 삽입하는 것에서 outerHTML에 삽입하는 것으로
    수정하여, include-html 속성을 적었던 껍질 태그를 남기지 않고 불러온 HTML 요소들만 DOM에 남도록 했다.
    - animate.js: 기존의 index.js 파일로 함수 내용은 동일하다. 하지만 window의 load 이벤트를 감지하는
    부분에서 이슈가 있어서 이 부분에 대한 핸들링이 있었다. include-html의 모든 작업이 빠르게 끝나버리면,
    animate.js에서 window에 load에 대한 이벤트 리스너를 추가하기 이전에 document.readyState가
    'complete'로 변경되어 load 이벤트가 발생하지 않는 문제가 생긴다. 이 이슈는 새로고침 열 번에 한 번 정도 발생했다.
    이를 해결하기 위해, 먼저 document.readyState가 'complete'인지 확인하고, 이미 'complete'라면
    기존의 내용을 실행하고, 'complete'가 아니라면 document.onreadystatechange 이벤트 핸들러에 기존
    내용들을 등록하여 문제를 해결했다.
    - video transform: s5-video와 s7-video의 위치 조정 및 화면 비율에 따른 scale 수정

--- js/include-html.js
+++ js/include-html.js
@@ -1,4 +1,4 @@
-(async () => {
+export const includeHTML = async () => {
   const elements = document.querySelectorAll("[include-html]");
 
   for (const element of elements) {
@@ -9,16 +9,15 @@
         const response = await fetch(file);
         if (response.status === 200) {
           const html = await response.text();
-          element.innerHTML = html;
+          element.outerHTML = html;
         } else if (response.status === 404) {
           element.innerHTML = "페이지를 찾을 수 없습니다.";
         } else {
           console.log(response);
         }
-        element.removeAttribute("include-html");
       } catch (error) {
         console.error("오류가 발생했습니다:", error);
       }
     }
   }
-})();
+};
